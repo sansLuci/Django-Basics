9ef8e3834e90ceafe064854ec580e0cb21a765f8
Sanskar Jain <sanskarj123@gmail.com>
2020-06-28 16:56:04 +0530

    Hello view, urls in app

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+	""""""
+
+	def get(self, request, format=None):
+		"""Returns a list of APIView fratures"""
+
+		features = [
+			'Uses HTTP methods as function (get, post, put, patch, delete)',
+			'It is similar to a traditional Django view',
+			'Gives you the most control over your logic',
+			'Is mapped manually to the URLs'
+		]
+
+		return Response({'message':'Hello', 'features':features}) # passing a dictionary for json output
